6ed145c0c147d19861b3069b31409feff7d76e93
gujarats <gujarat.santana@gmail.com>
2017-09-10 13:44:39 +0700

    handle errors

--- stealer.go
+++ stealer.go
@@ -1,5 +1,7 @@
 package stealer
 
+import "errors"
+
 type Stealer struct {
 	Datas map[string][]string
 }
@@ -13,8 +15,17 @@ func Steal(path string) (error, *Stealer) {
 }
 
 // save all the variables and its values to new path
+// TODO : write test case where the path is not exist and need to create the path first
+// lets say path/to/specific/file.go need to create the folder first
 func (s *Stealer) Save(path, packageName string) error {
 	var err error
+	if s.Datas == nil {
+		err = errors.New("Stealer Datas are empty")
+		return err
+	}
 	err = WriteFile(path, packageName, s.Datas)
-	return err
+	if err != nil {
+		return err
+	}
+	return nil
 }
